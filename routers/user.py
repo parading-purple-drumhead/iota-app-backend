c75ce820b1d768dfb966ab5d042185348aa45120
kbansal77 <kartikbansal2000@gmail.com>
2020-07-09 17:38:01 +0530

    added get useinfo and add function

--- routers/user.py
+++ routers/user.py
@@ -15,13 +15,29 @@ def get_userInfo(user_id):
         print(e)
         raise HTTPException(status_code=400, detail=str(e))
 
-@router.post("/add")
-def add_post(user: User):
+@router.put("/{user_id}")
+def edit_user(user_id,user:User):
     try:
-        print("new test here")
-        doc_ref =  db.collection(u"users")
-        doc_ref.add(dict(user))
+        edit=db.collection(u"users").document(user_id)
+        edit.update(dict(user))
+    except Exception as e:
+        print(e)
+        raise HTTPException(status_code=400, detail=str(e))
 
+@router.post("/{user_id}/add")
+def add_post(user_id,user: User):
+    try:
+        doc_ref =  db.collection(u"users")
+        doc_ref.add(dict(user),document_id=user_id)
+
+    except Exception as e:
+        print(e)
+        raise HTTPException(status_code=400, detail=str(e))
+
+@router.delete("/{user_id}")
+def delete_user(user_id):
+    try:
+        db.collection(u"users").document(user_id).delete()
     except Exception as e:
         print(e)
         raise HTTPException(status_code=400, detail=str(e))
