0c01555e6d541fad725ac0ced8887187130b9fa6
kbansal77 <kartikbansal2000@gmail.com>
2020-07-09 18:02:27 +0530

    added get useinfo and add function

--- routers/user.py
+++ routers/user.py
@@ -1,8 +1,9 @@
 from fastapi import APIRouter, HTTPException
 from models import User
-from routers import db, tokenverify
+from routers import db
 
 router = APIRouter()
+
 
 @router.get("/{user_id}")
 def get_userInfo(user_id):
@@ -15,24 +16,27 @@ def get_userInfo(user_id):
         print(e)
         raise HTTPException(status_code=400, detail=str(e))
 
+
 @router.put("/{user_id}")
-def edit_user(user_id,user:User):
+def edit_user(user_id, user: User):
     try:
-        edit=db.collection(u"users").document(user_id)
+        edit = db.collection(u"users").document(user_id)
         edit.update(dict(user))
     except Exception as e:
         print(e)
         raise HTTPException(status_code=400, detail=str(e))
 
+
 @router.post("/{user_id}/add")
-def add_post(user_id,user: User):
+def add_post(user_id, user: User):
     try:
-        doc_ref =  db.collection(u"users")
-        doc_ref.add(dict(user),document_id=user_id)
+        doc_ref = db.collection(u"users")
+        doc_ref.add(dict(user), document_id=user_id)
 
     except Exception as e:
         print(e)
         raise HTTPException(status_code=400, detail=str(e))
+
 
 @router.delete("/{user_id}")
 def delete_user(user_id):
