57f21c09ce2520ab97e5da45fa26917192622d57
kbansal77 <kartikbansal2000@gmail.com>
2020-07-19 21:15:25 +0530

    removed send message function init.py

--- routers/__init__.py
+++ routers/__init__.py
@@ -11,12 +11,3 @@ def tokenverify(safe):
     decoded_token = auth.verify_id_token(safe)
     uid = decoded_token["uid"]
     return uid
-
-
-def send_message_to_topic(notification, data, topic):
-    try:
-        message = messaging.Message(notification=notification, data=data, topic=topic)
-        response = messaging.send(message)
-        print(response)
-    except Exception as e:
-        return (e)
