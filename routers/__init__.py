75199d60d9b82451dc8eaeae994bd98f7631e9c2
kbansal77 <kartikbansal2000@gmail.com>
2020-07-20 20:36:23 +0530

    Updated User Class; Add , Edit and Delete comment

--- routers/__init__.py
+++ routers/__init__.py
@@ -1,5 +1,5 @@
 import firebase_admin
-from firebase_admin import auth, credentials, firestore, messaging
+from firebase_admin import auth, credentials, firestore
 
 cred = credentials.Certificate("./serviceAccountKey.json")
 firebase_admin.initialize_app(cred)
